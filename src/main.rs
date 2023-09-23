a00a6545cf57b44e8cb935841d631c1419972945
Goobyster <jarpe2@hotmail.fi>
2023-09-23 22:09:24 +0300

    added command line arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 mod database;
 mod kf2;
+mod model;
 mod parse;
 
+use clap::Parser;
 use kf2::Kf2Logger;
+use url::Url;
+
+#[derive(Debug, Parser)]
+struct Args {
+    server_ip: Url,
+    username: String,
+    password: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let kf2 = Kf2Logger::new_session(
-        "http://192.168.1.7:8081".to_string(),
-        "".to_string(),
-        "".to_string(),
-    )
-    .await
-    .unwrap();
+    let args = Args::parse();
+    let kf2 = Kf2Logger::new_session(args.server_ip, args.username, args.password)
+        .await
+        .unwrap();
+    kf2.log_players().await.unwrap();
 }
